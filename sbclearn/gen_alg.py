81f16bc33ee83d15b93c67967045fec8dc949b22
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-01-25 14:17:29 +0000

    Updated to support generic hyperparameter optimisation.

--- sbclearn/gen_alg.py
+++ sbclearn/gen_alg.py
@@ -12,15 +12,15 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 import synbiochem.optimisation.gen_alg as gen_alg
 
 
-class ClassifierGeneticAlgorithm(gen_alg.GeneticAlgorithm):
+class LearnGeneticAlgorithm(gen_alg.GeneticAlgorithm):
     '''Class to optimise parameters for Classifier using a GA.'''
 
     def __init__(self, pop_size, predicter, args,
                  retain=0.2, random_select=0.05, mutate=0.01, verbose=False):
         '''Constructor.'''
-        super(ClassifierGeneticAlgorithm, self).__init__(pop_size, args,
-                                                         retain, random_select,
-                                                         mutate, verbose)
+        super(LearnGeneticAlgorithm, self).__init__(pop_size, args,
+                                                    retain, random_select,
+                                                    mutate, verbose)
 
         self.__predicter = predicter
 
@@ -31,20 +31,16 @@ class ClassifierGeneticAlgorithm(gen_alg.GeneticAlgorithm):
         num_hidden_layers = individual.pop('num_hidden_layers', 1)
         activ_func = individual.pop('activ_func', 'relu')
         num_nodes = individual.pop('num_nodes', 128)
-        individual['hidden_layers'] = [(num_nodes, activ_func)] * \
-            num_hidden_layers
+        hidden_layers = [(num_nodes, activ_func)] * num_hidden_layers
 
-        cls = self.__predicter.predict(**individual)
+        return_vals = self.__predicter.predict(hidden_layers, individual)
 
         # Reform individual dict:
-        individual.pop('hidden_layers')
         individual['num_hidden_layers'] = num_hidden_layers
         individual['activ_func'] = activ_func
         individual['num_nodes'] = num_nodes
 
         if self._verbose:
-            print str(cls[3])
-            print str(cls[4]) + '\t' + str(individual)
-            print
+            print ('%.3f\t' % return_vals[0]) + str(individual)
 
-        return 1 - cls[4]
+        return return_vals[0]
