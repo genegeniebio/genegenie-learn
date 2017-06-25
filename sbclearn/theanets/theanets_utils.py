523a9158bf1ccd41d19b5ba837be24dc4346491c
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-06-25 14:14:29 +0100

    Made methods more generic.

--- sbclearn/theanets/theanets_utils.py
+++ sbclearn/theanets/theanets_utils.py
@@ -8,6 +8,7 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 @author:  neilswainston
 '''
 # pylint: disable=too-few-public-methods
+# pylint: disable=too-many-arguments
 from collections import defaultdict
 
 from sklearn import model_selection
@@ -51,15 +52,14 @@ class Classifier(TheanetsBase):
     '''Simple classifier in Theanets.'''
 
     def __init__(self, x_data, y_data):
-        super(Classifier, self).__init__(theanets.Classifier, x_data,
-                                         y_data.astype(np.int32))
+        self.__num_outputs = len(set(self._y_data))
+        y_data = np.array([[y] for y in y_data.astype(np.int32)])
+        super(Classifier, self).__init__(theanets.Classifier, x_data, y_data)
 
     def train(self, valid_size=0.25, hidden_layers=None, hyperparams=None):
         '''Train classifier.'''
-        num_outputs = len(set(self._y_data))
-
-        super(Classifier, self)._train(num_outputs, valid_size, hidden_layers,
-                                       hyperparams)
+        super(Classifier, self)._train(self.__num_outputs, valid_size,
+                                       hidden_layers, hyperparams)
 
     def predict(self, x_test):
         '''Classifies test data.'''
@@ -85,20 +85,24 @@ class Regressor(TheanetsBase):
         return self._exp.network.predict(x_test)
 
 
-def k_fold_cross_valid((x_data, y_data), tests=50, test_size=0.05,
+def k_fold_cross_valid((x_data, y_data), regression=True,
+                       tests=50, test_size=0.05,
                        hidden_layers=None, hyperparams=None):
     '''k-fold cross validation.'''
     results = defaultdict(list)
 
     for _ in range(tests):
-        x_train, x_test, y_train, y_test = \
+        data = \
             model_selection.train_test_split(x_data, y_data,
                                              test_size=test_size)
 
-        regressor = Regressor(x_train, y_train)
-        regressor.train(hidden_layers=hidden_layers, hyperparams=hyperparams)
+        model = Regressor(data[0], data[2]) \
+            if regression \
+            else Classifier(data[0], data[2])
 
-        for test, pred in zip(y_test, regressor.predict(x_test)):
+        model.train(hidden_layers=hidden_layers, hyperparams=hyperparams)
+
+        for test, pred in zip(data[3], model.predict(data[1])):
             results[test].append(pred[0])
 
     return results
