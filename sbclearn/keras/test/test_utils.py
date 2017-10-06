f9e13984c71f96f12980cec17103d1b482f9d164
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-10-06 12:15:27 +0100

    Added keras test.

--- sbclearn/keras/test/test_utils.py
+++ sbclearn/keras/test/test_utils.py
@@ -7,18 +7,25 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
+# pylint: disable=invalid-name
 # pylint: disable=no-member
 import unittest
 
+from keras.layers import Dense
+from keras.models import Sequential
 from keras.utils import to_categorical
+from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn import metrics, model_selection
 from sklearn.datasets.samples_generator import make_blobs
-# from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 
 from sbclearn.keras.utils import Classifier
 import numpy as np
+import pandas as pd
 
 
+# from sklearn.metrics import confusion_matrix
 class TestClassifier(unittest.TestCase):
     '''Tests the Classifier class.'''
 
@@ -46,3 +53,43 @@ class TestClassifier(unittest.TestCase):
         # print confusion_matrix(y_test, y_pred)
 
         self.assertTrue(metrics.accuracy_score(y_test, y_pred) > 0.9)
+
+
+class TestRegressor(unittest.TestCase):
+    '''Tests the Regressor class.'''
+
+    def test_regression(self):
+        '''Tests the regression method.'''
+        df = pd.read_csv('housing.csv', delim_whitespace=True,
+                         header=None)
+        dataset = df.values
+        x_data = dataset[:, 0:-1]
+        y_data = dataset[:, -1]
+
+        # create model
+
+        # fix random seed for reproducibility
+        seed = 7
+        np.random.seed(seed)
+        # evaluate model with standardized dataset
+        regressor = KerasRegressor(
+            build_fn=get_model, nb_epoch=100, batch_size=5)
+
+        kfold = KFold(n_splits=10, random_state=seed)
+        results = cross_val_score(regressor, x_data, y_data, cv=kfold)
+        print "Results: %.2f (%.2f) MSE" % (results.mean(), results.std())
+
+        self.assertTrue(results.mean() > 50)
+
+
+def get_model():
+    '''Gets model.'''
+    model = Sequential()
+    model.add(Dense(64,
+                    input_dim=13,
+                    kernel_initializer='normal',
+                    activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
