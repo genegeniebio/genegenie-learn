633762ef5ebc50c36075cc8a4c9a95a3a9423401
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-01-09 13:50:26 +0000

    Added filtering of source.

--- sbclearn/nicole/nicole.py
+++ sbclearn/nicole/nicole.py
@@ -23,7 +23,9 @@ def get_data(xl_filename):
 
     for dirpath, _, filenames in os.walk(dir_name):
         for filename in filenames:
-            dfs.append(pd.read_csv(os.path.join(dirpath, filename)))
+            df = pd.read_csv(os.path.join(dirpath, filename))
+            df['source'] = filename[:-4]
+            dfs.append(df)
 
     df = pd.concat(dfs)
     df.set_index('id', inplace=True)
@@ -33,27 +35,40 @@ def get_data(xl_filename):
     return df.drop_duplicates()
 
 
-def analyse(df):
-    '''Analyse.'''
+def align(df, sources=None):
+    '''Align.'''
+    # Filter rows:
+    filt_df = _filter(df, sources)
 
     # Perform Clustal Omega alignment:
-    df['align_seq'] = pd.Series(seq_utils.do_clustal(df.to_dict()['seq']))
+    filt_df['align_seq'] = \
+        pd.Series(seq_utils.do_clustal(filt_df.to_dict()['seq']))
 
     # Strip out positions with identical residues:
-    aas = [list(seq) for seq in df['align_seq']]
+    aas = [list(seq) for seq in filt_df['align_seq']]
     char_df = pd.DataFrame(aas,
                            columns=['pos_' + str(val)
                                     for val in range(0, len(aas[0]))],
-                           index=df.index)
+                           index=filt_df.index)
     char_df = char_df.loc[:, (char_df != char_df.iloc[0]).any()]
 
-    df['dif_align_seq'] = char_df.apply(''.join, axis=1)
+    filt_df['dif_align_seq'] = char_df.apply(''.join, axis=1)
+
+    return filt_df
+
+
+def _filter(df, sources):
+    '''Filter by source.'''
+    if sources:
+        return df.loc[df['source'].isin(sources)]
+
+    return df
 
 
 def main(args):
     '''main method.'''
     df = get_data(args[0])
-    analyse(df)
+    df = align(df, args[1:] if len(args) > 1 else None)
     df.to_csv('out.csv')
 
 
